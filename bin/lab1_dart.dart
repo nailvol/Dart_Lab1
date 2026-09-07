01fbeb6b4e7d12a0579e373c9339e2af601b3cef
nailvol <nail34rus@gmail.com>
2026-09-07 13:29:03 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,12 +1,27 @@
 void main() {
+String? city = null;
+if (city != null){
+  print(
+    city.toUpperCase()
+    );
+  print(city?.toUpperCase());
+}
+String? nickname = null;
+String display=nickname ?? 'Аноним';
+print(display);
+
+const String appName = 'Lab1';
+final int startYear = 2026;
+print('$appName started in $startYear');
+var score = 95;
+var language = 'Dart';
+print ('$language: $score');
 String name = "Наиль";
 int age = 18;
 double height = 1.83;
 bool isStudent = true;
 
-print (name);
-print (age);
-print (height);
-print (isStudent);
-print (name);
+print ('Привет, $name! Тебе $age лет');
+print('через 5 лет тебе будет ${age + 5} лет.');
+
 }
